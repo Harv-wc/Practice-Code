175410cdde7f3a29d92faddcfaef96ec9750534d
William <harv.wc@gmail.com>
2019-08-13 08:37:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSquare DoMath = new SortedSquare { };
+            int[] arr = new int[] { -9, -2, 0, 2, 3 };
+            Console.WriteLine($"Original Array: {DoMath.ToStringAction(arr)}");
+            Console.WriteLine($"Sorted Squared Array: {DoMath.ToStringAction(DoMath.SortArray(DoMath.SquareArray(arr)))}");
+
+
+
+
+            // PAUSE //
+            Console.ReadLine();
+        }
+       
+    }
+    
+    class SortedSquare
+    {
+        public int[] SortArray(int[] array)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                int temp = 0;
+                for(int nextIndex = index+1; nextIndex < array.Length; nextIndex++)
+                {
+                    if(array[nextIndex]<array[index])
+                    {
+                        temp = array[index];
+                        array[index] = array[nextIndex];
+                        array[nextIndex] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        public int[] SquareArray(int[] array)
+        {
+            
+            for (int index = 0; index < array.Length; index++)
+            {
+                array[index] = (array[index] * array[index]);
+            }
+            return array;
+        }
+        public string ToStringAction(int[] array)
+        {
+            string asString = "";
+            foreach(int num in array)
+            {
+                asString += (Convert.ToString(num) + " ");
+            }
+            return asString;
+        }
+
+
+    } //Given a sorted list of integers, square the elements and give the output in sorted order.
+
+    
+}
---
+++ Storage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Environment
+{
+    class Storage
+    {
+        public void Menu()
+        {
+            // Change the number of menu items here
+            const int quitVariable = 3;
+            int selector = new int();
+            bool userInput = false;
+            int size = new int();
+
+            while (selector != quitVariable)
+            {
+                Console.Clear();
+                DrawMenu();
+                userInput = int.TryParse(Console.ReadLine(), out selector);
+                if (userInput)
+                {
+                    switch (selector)
+                    {
+                        case 1:
+                            // EASY MODE
+                            size = 8;
+                            selector = 3;
+                            break;
+                        case 2:
+                            //HARD MODE
+                            size = 11;
+                            selector = 3;
+                            break;
+                        case 3:
+                            //QUIT GAME
+                            userInput = false;
+                            break;
+                        //  If more cases, add here. If so, go to the top and change maxMenuItems amount the total cases 
+                        default:
+                            break;
+                    }
+                }
+            }
+            while (userInput == true)
+            {
+                // GAME HERE
+                Console.WriteLine("Game plays");
+                Console.ReadLine();
+            }
+        }
+        private static void DrawMenu()
+        {
+            Console.WriteLine(" Welcome To STAR COLLECTOR ");
+            Console.WriteLine("****************************");
+            Console.WriteLine(" Select Your Difficulty ");
+            Console.WriteLine("****************************");
+            Console.WriteLine(" 1. REGULAR");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" 2. HARD");
+            Console.ResetColor();
+            Console.WriteLine(" 3. Quit Game");
+            Console.WriteLine("****************************");
+        }
+
+    }
+}
