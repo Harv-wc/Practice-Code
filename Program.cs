a9029e72b2d9d0c91a9422c6b123ef6aaf220d05
William <harv.wc@gmail.com>
2019-08-16 15:05:01 -0400

    Calc max profit potential

--- Program.cs
+++ Program.cs
@@ -8,7 +8,26 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            MaxProfit();
             
         }
+
+        public static int MaxProfit()
+        {
+            int[] stocks = { 9, 11, 8, 5, 7, 10, 1, 2, 3, 4 };
+            int profit = 0;
+            for (int i = 0; i < stocks.Length; i++)
+            {
+                for (int i2 = i + 1; i2 < stocks.Length; i2++)
+                {
+                    if ((stocks[i2] - stocks[i]) > profit)
+                    {
+                        profit = (stocks[i2] - stocks[i]);
+                    }
+                }
+            }
+            Console.WriteLine(profit);
+            return profit;
+        }// 
     }
 }
